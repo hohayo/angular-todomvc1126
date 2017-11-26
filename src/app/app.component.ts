00a3664d4d6ca1d357dcabcc9859378325badd9e
Jerry Lin <jerry.jzlin@gmail.com>
2017-11-26 11:04:47 +0800

    任務 06 進階練習：當欄位沒有輸入任何資料直接按下 Enter 時，請不要新增空值到 todos 陣列中

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,9 @@ export class AppComponent {
 
   addTodo(element: HTMLInputElement) {
     // console.log(element.value);
-    this.todos.push(element.value);
+    if (element.value) {
+      this.todos.push(element.value);
+    }
   }
 
 }
